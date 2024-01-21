765109e05c71012dd544f2569d023138b9f1bd9b
carlosgomezchico <145378795+carlosgomezchico@users.noreply.github.com>
2024-01-21 13:07:22 +0000

    Update Currency.java

--- src/main/java/Currency.java
+++ src/main/java/Currency.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Currency {
+package PACKAGE_NAME;
+public class Currency {
+    private final String code;
+    private final String name;
+    private final String symbol;
+
+    public Currency(String code, String name, String symbol) {
+        this.code = code;
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    @Override
+    public String toString() {
+        return "Currency{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", symbol='" + symbol + '\'' +
+                '}';
+    }
 }
